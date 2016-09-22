1938a8a49cdabbe98c3ac77dcbd826b7d180260c
Xandro Hemmelder <xandro.hemmelder@takeaway.com>
2016-09-22 15:40:18 +0200

    Removed specific config from site.pp.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,11 +1,3 @@
 node default {
   hiera_include('classes')
-  hiera_resources('hiera_create_types')
-}
-
-node /^chief\.500k\.lan$/ {
-  class { 'backuppc::client':
-    rsync_share_name => ['/'],
-    backup_files_exclude => [ 'home', 'opt/kvm', 'proc', 'dev', 'sys' ],
-  }
 }
